8d97bb8202656a6281f1dae5c9a3bf5c829759cb
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-20 18:48:07 +0100

    Allow users to delete Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,18 +17,44 @@ class RecipesController < ApplicationController
     @recipe = set_recipe
     @recipe_foods = @recipe.recipe_foods.includes(:food).where(recipe_id: @recipe.id)
   end
-end
 
-def new
-  @recipe = Recipe.new
-end
+  def new
+    @recipe = Recipe.new
+  end
 
-def create
-  @recipe = Recipe.new(recipe_params)
-  @recipe.user = current_user
-  if @recipe.save
-    redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.'
-  else
-    render :new, status: :unprocessable_entity
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: params[:recipe][:public])
+    respond_to do |format|
+      format.json { render json: { success: true } }
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods
+    @recipe_foods.each(&:destroy)
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
   end
 end
