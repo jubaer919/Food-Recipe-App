f7d3991b472d986215fa5e8421727b101f5b7d60
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-12-20 21:09:43 +0600

    Showing the list of foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,8 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: %i[show destroy]
+  before_action :authenticate_user!
+
+  def index
+    @foods = Food.where(user_id: current_user.id)
+  end
 end
