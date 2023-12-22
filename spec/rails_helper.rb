a74efe473e6555c30b0066d0957332be8a7821ea
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-22 09:31:22 +0100

    Add unit test for user models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,9 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -39,6 +41,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
