79ff8efe54b5ed6ce334e1f38752719ca48f49b5
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-12-22 21:59:03 +0600

    resolve the the code-reviewer issues

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  validates :name, :measurement_unit, :quantity, :price, presence: true
+  validates :name, :measurement_unit, :price, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
