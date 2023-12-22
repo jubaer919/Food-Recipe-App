80ad9912acd1cd50a897fce0ee8a2c56ab55e3ff
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-12-22 10:16:51 +0600

    fix some issues

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,18 +1,22 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[show destroy]
+  before_action :set_food, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
   def index
     @foods = Food.where(user_id: current_user.id)
   end
 
-  def show; end
+  def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @food }
+    end
+  end
 
   def new
     @food = Food.new
   end
 
-  # POST /foods or /foods.json
   def create
     @food = Food.new(food_params)
     @food.user_id = current_user.id
@@ -23,20 +27,35 @@ class FoodsController < ApplicationController
     end
   end
 
-  # DELETE /foods/1 or /foods/1.json
+  def edit
+    # This action is used to render the edit form
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to food_url(@food), notice: 'Food was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
-    @food.destroy
-    redirect_to food_url, notice: 'Food was successfully destroyed.'
+    @food = Food.find_by(id: params[:id])
+
+    if @food
+      @food.destroy
+      redirect_to foods_url, notice: 'Food was successfully destroyed.'
+    else
+      redirect_to foods_url, alert: 'Food not found.'
+    end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_food
     @food = Food.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
